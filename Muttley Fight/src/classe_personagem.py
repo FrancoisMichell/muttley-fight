749fb970662e45c8f456dd918024c61c6040c6d5
jricardoborba@gmail.com <jricardoborba@gmail.com@62d446ae-3e79-5876-ff7c-d07800681c61>
2014-01-31 12:03:08 +0000

    Esboço da classe personagem

---
+++ Muttley Fight/src/classe_personagem.py
@@ -0,0 +1,25 @@
+# esboço da classe personagem, com atributos, vida, essas coisas.
+
+class Personagem:
+    def __init__(self,vida):
+        
+        self.vida=vida
+        self.sprites={}
+        def Soco:
+            
+        def Chute:
+            
+        def andar_direita:
+            
+        def andar_esquerda:
+            
+        def pular:
+            
+        def especial:
+            
+        def defesa:
+            
+        def provocar:
+            
+        def morte:
+            if self.vida==0:
